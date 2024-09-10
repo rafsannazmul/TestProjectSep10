2d06247a05599f3f2415fb993483a3ff332812e1
rafsannazmul <rafsannazmul@gmail.com>
2024-09-10 22:03:26 +0600

    adding necessary files

---
+++ Read.md
@@ -0,0 +1,10 @@
+#  Chat Project
+
+ℹ️ This is a very simple chat app made using Firebase Cloud Messaging.
+
+ℹ️ Please refresh the swift package manager caches before building
+
+ℹ️ For development, I have used storyboard, autolayout constraints and Swift as programming language. For adding Firebase in the project I have used Swift Package Manager.
+
+ℹ️ I have integrated FCM but I do no have a paid developer account. So cound not add "Push Notification" capabilities in the project. To test that needs to be implemented. However I have added a notification.apns file that can be dragged in the simulator the check whether push notification is working or not.
+
--- TestProjectSep10/ViewController.swift
+++ TestProjectSep10/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var messageWriteView: UIView!
+    
+    @IBOutlet weak var messageWriteViewHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var messageWriteViewBottomConstraint: NSLayoutConstraint!
+    
+    
+    // MARK: View Loading Actions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Notification Center Observers
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-
+    
+    // MARK: IBActions
+    
+    // MARK: Actions
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if messageWriteViewBottomConstraint.constant == 0 {
+                messageWriteViewBottomConstraint.constant = keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if messageWriteViewBottomConstraint.constant != 0 {
+            messageWriteViewBottomConstraint.constant = 0
+        }
+    }
+    
+    
 }
 
